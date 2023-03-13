5ee63e2434fe3e152b2aceb683f79bc5f5ef8dc2
Shantanu Parab <112659509+shantanuparabumd@users.noreply.github.com>
2023-03-12 21:54:52 -0400

    Final Commit

--- dijkstra.py
+++ dijkstra.py
@@ -1,14 +1,15 @@
 # Imports
-import pygame
 import math
 import numpy as np
 import time
 from queue import PriorityQueue
-
+import cv2
 
 class Dijkstra:
     def __init__(self,width,height,scale,start,goal):
-        pygame.init()
+        # Get the video dimensions and FPS
+        
+        self.result = cv2.VideoWriter("Dijkstra01.avi", cv2.VideoWriter_fourcc(*'MJPG'), 600, (width, height))
         
         # Define constants
         self.START=start
@@ -16,144 +17,200 @@ class Dijkstra:
         self.SCALE=scale
         self.WIDTH=width
         self.HEIGHT=height
-        self.screen_size=self.get_screen_size(width,height,scale)
-        self.screen = pygame.display.set_mode(self.screen_size)
+        # self.screen_size=self.get_screen_size(width,height,scale)
+        # self.screen = pygame.display.set_mode(self.screen_size)
 
         # Define Colors
         self.background=(255,255,255)
         self.obst_color=(0,0,0)
-        self.clear_color=(255, 255, 245)
+        self.clear_color= (145, 194, 245)
         self.grid_color=(233, 226, 230, 0.2)
         self.start_color=(132, 222, 15, 0.8)
         self.goal_color=(254, 0, 0, 0.8)
-        self.explored_color= (237, 188, 64)
-        self.track_color= (252, 184, 100, 1)
-        self.robot_color= (168, 230, 53, 1)
+        self.explored_color= (64, 188, 237)
+        self.track_color= (64, 64, 237)
+        self.robot_color= (168, 230, 53)
 
         # Define Grid Size
-        self.grid_size=5
+        self.grid_size=2
 
         self.H_PX=self.WIDTH*self.SCALE/self.grid_size
         self.V_PX=self.HEIGHT*self.SCALE/self.grid_size
 
-        
+        self.HEX=self.hexagon(125,300,50)
+        self.HEX_CLR=self.hexagon(125,300,60)
+        # Triangle
+        p1=((250-20),455)
+        p2=(20, 455)
+        p3=((250-20),465)
+        p4=(20, 465)
+        p5=(125,515)
+        self.TRI_CLR = [p1, p2, p4, p5,p3]
+        p1=(25,460)
+        p2=(225, 460)
+        p3=(125,510)
+        self.TRI=[p1,p2,p3]
     
-    # Main Loop
+    def line_equation(self,p,p1,p2):
+        try:
+            m=(p2[1]-p1[1])/(p2[0]-p1[0])
+            value=p[1]-p1[1]-(m*p[0])+(m*p1[0])
+        except:
+            value=p[0]-p1[0]
+            
+        return value
+    
+    def hexagon(self,cx, cy, sl):
+            # Calculate the points of the hexagon
+            center_x = cx
+            center_y = cy
+            side_length = sl
+            angle_deg = 60
+            angle_rad = math.radians(angle_deg)
+            angle_deg = 60
+            angle_rad = math.radians(angle_deg)
+            points = []
+            for i in range(6):
+                x = round(center_x + side_length * math.cos(angle_rad * i))
+                y = round(center_y + side_length * math.sin(angle_rad * i))
+                points.append((x, y))
+            return points
+    
+    def obstacle_detect_triangle(self,p,v1,v2,v3):
+        h1=self.line_equation(p,v2,v3)>=0 if self.line_equation(v1,v2,v3)>0 else self.line_equation(p,v2,v3)<=0
+        h2=self.line_equation(p,v1,v3)>=0 if self.line_equation(v2,v1,v3)>0 else self.line_equation(p,v1,v3)<=0
+        h3=self.line_equation(p,v1,v2)>=0 if self.line_equation(v3,v1,v2)>0 else self.line_equation(p,v1,v2)<=0
+        print(h1,h2,h3)
+        return h1 and h2 and h3
+
+    def obstacle_detect_rectangle(self,point,x,y,width,height):
+        if point[0]>=x and point[1]>=y and point[0]<=(x+width) and point[1]<=(y+height):
+            return True
+        else:
+            return False
+    
+    def obstacle_detect_boundary(self,point,x,y,width,height):
+        if point[0]<=x or point[1]<=y or point[0]>=(x+width) or point[1]>=(y+height):
+            return True
+        else:
+            return False
+        
+    def obstacle_detect_polygon(self,p,points):
+        points=points+points[:3]
+        for i in range(len(points)-3):
+            v1,v2,v3=points[i],points[i+1],points[i+2]
+            h=self.line_equation(p,v1,v2)>=0 if self.line_equation(v3,v1,v2)>0 else self.line_equation(p,v1,v2)<=0
+            if not h:
+                return False
+        return True
+
+            
+    def check_obstacle(self,p):
+        h1=self.obstacle_detect_rectangle(p,0,100,100,50)
+        h2=self.obstacle_detect_rectangle(p,150,100,100,50)
+        h3=self.obstacle_detect_polygon(p,self.HEX)
+        h4=self.obstacle_detect_polygon(p,self.TRI)
+
+        return (h1 or h2 or h3 or h4 )
+
+    
+    def check_clearance(self,p):
+        h1=self.obstacle_detect_rectangle(p,0,95,105,60)
+        h2=self.obstacle_detect_rectangle(p,145,95,105,60)
+        h3=self.obstacle_detect_polygon(p,self.HEX_CLR)
+        h4=self.obstacle_detect_polygon(p,self.TRI_CLR)
+        h5= self.obstacle_detect_boundary(p,5,5,240,590)
+        
+        return (h1 or h2 or h3 or h4 or h5)
+
+    def make_obstacle_space(self):
+        grid=np.ones((self.HEIGHT,self.WIDTH,3),np.uint8)
+        nodes=[]
+        flag=0
+        for i in range(self.HEIGHT):
+            row=[]
+            for j in  range(self.WIDTH):
+                if self.check_clearance((i,j)):
+                    row.append([-1,None,None,(i,j)])
+                    grid[i][j]=np.array(self.clear_color)
+                    if self.check_obstacle((i,j)):
+                        grid[i][j]=np.array(self.obst_color)
+                else:
+                    row.append([float('inf'),None,None,(i,j)])
+                    grid[i][j]=np.array(self.background)
+            nodes.append(row)
+        return grid,nodes
+    
     def game(self):
         self.running=True
-        self.drawgrid()
-        self.draw_obstacles()
-        if self.check_grid(self.GOAL[0],self.GOAL[1]):
-            self.change_color(self.goal_color,self.GOAL[0],self.GOAL[1])
-            print("Invalid")
-            pygame.display.update()
-            time.sleep(6)
-            self.running=False
+        if self.START[0]>=0 and self.START[0]<self.HEIGHT and self.START[1]>=0 and self.START[1]<self.WIDTH:
+            if self.check_obstacle(self.START):
+                print("Invalid Start")
+                self.running=False
+            else:
+                print("Valid Start")
         else:
-            print("Valid")
-            tracker,current=self.dijkstra()
-            self.back_track(tracker,current)
-        while self.running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running=False
-        # quit pygame
-        pygame.quit()
+            self.running=False
+            print(" Start Out of Bounds")
 
-    def get_screen_size(self,width,height,scale):
-    #     10pixel =1mm
-        screen_size=(width*scale,height*scale)
-        
-        return screen_size
-    def drawgrid(self):
-        for row in range(int(self.V_PX)):
-            for col in range(int(self.H_PX)):
-                # Calculate the position and size of the current cell
-                x = col * self.grid_size
-                y = row * self.grid_size
-                cell_rect = pygame.Rect(x, y, self.grid_size, self.grid_size)
+        if self.GOAL[0]>=0 and self.GOAL[0]<self.HEIGHT and self.GOAL[1]>=0 and self.GOAL[1]<self.WIDTH:
+            if self.check_obstacle(self.GOAL):
+                print("Invalid")
+                self.running=False
+            else:
+                print("Valid")
+        else:
+            self.running=False
+            print(" Goal Out of Bounds")
+        if self.running:
+            self.img,self.node_grid = self.make_obstacle_space()
+            print(len(self.node_grid),len(self.node_grid[0]))
+            self.t,self.c= self.dijkstra()
+            print(self.c)
+            print(self.t[0])
+            self.img=self.back_track(self.t,self.c,self.img)
+            self.img = cv2.flip(self.img, 0)
+            for i in range(3000):
+                self.result.write(self.img)
+            cv2.imshow("Final Output", self.img)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 
-                # Draw the cell as a white box with a grey border
-                pygame.draw.rect(self.screen, self.grid_color, cell_rect, 1)
-                pygame.draw.rect(self.screen, self.background, cell_rect.inflate(-1, -1))
+    def back_track(self,tracker,current,img):
+        g_node=current
+        path=[]
+        while not g_node[2]==0:
+            for c in tracker:
+                if c[2] == g_node[1]:
+                    path.append([c[3][0],c[3][1]])
+                    g_node=c
+        print("Complete")
+        path.reverse()
+        for x,y in path:
+            img[x][y]=np.array(self.track_color)
+        return img
 
-    def draw_triangle(self,p1,p2,p3, color):
-        # Define the vertices of the triangle
-        vertices = [p1, p2, p3]
-        # Draw the triangle
-        pygame.draw.polygon(self.screen, color , vertices)
+    def move(self,current,action):
+        i=current[0]+action[0]
+        j=current[1]+action[1]
+        if i>=0 and j>=0 and i<self.HEIGHT and j<self.WIDTH:
+            cost=0
+            if action[0]==0 or action[1]==0:
+                cost=1
+            else:
+                cost=1.4
+            return self.node_grid[i][j],cost
+        else:
+            return None,None
     
-    def draw_heaxagon(self, cx, cy, sl, color):
-        # Calculate the points of the hexagon
-        center_x = cx*self.SCALE
-        center_y = cy*self.SCALE
-        side_length = sl*self.SCALE
-        angle_deg = 60
-        angle_rad = math.radians(angle_deg)
-        angle_deg = 60
-        angle_rad = math.radians(angle_deg)
-        points = []
-        for i in range(6):
-            x = round(center_x + side_length * math.cos(angle_rad * i))
-            y = round(center_y + side_length * math.sin(angle_rad * i))
-            points.append((x, y))
-
-        # Draw and fill the hexagon
-        pygame.draw.polygon(self.screen, color, points, 0)
-        
-    def draw_rectangle(self, x, y, w, h, color):
-        x,y,w,h=x*self.SCALE,y*self.SCALE,w*self.SCALE,h*self.SCALE
-        rect = pygame.Rect(x, y, w, h)
-        pygame.draw.rect(self.screen, color, rect)
-
-    def draw_obstacles(self):
-        # Draw Clearance
-        
-        self.draw_heaxagon(300,125,60, self.clear_color)
-        # Triangle
-        p1=(455*self.SCALE,(250-20)*self.SCALE)
-        p2=(455*self.SCALE,20*self.SCALE)
-        p3=(465*self.SCALE,(250-20)*self.SCALE)
-        p4=(465*self.SCALE,20*self.SCALE)
-        p5=(515*self.SCALE,125*self.SCALE)
-        vertices = [p1, p2, p4, p5,p3]
-        pygame.draw.polygon(self.screen, self.clear_color, vertices)
-
-        self.draw_rectangle(100-5,0-5,50+10,100+10, self.clear_color)
-        self.draw_rectangle(100-5,250-100-5,50+10,100+10, self.clear_color)
-
-        # Draw obstacles
-        self.draw_heaxagon(300,125,50, self.obst_color)
-        p1=(460*self.SCALE,(250-25)*self.SCALE)
-        p2=(460*self.SCALE,25*self.SCALE)
-        p3=(510*self.SCALE,125*self.SCALE)
-        self.draw_triangle(p1,p2,p3,self.obst_color)
-        self.draw_rectangle(100,0,50,100, self.obst_color)
-        self.draw_rectangle(100,250-100,50,100, self.obst_color)
-
-        pygame.display.update()
-
-    def check_grid(self,x,y):
-        for i in range(y*self.grid_size+1,y*self.grid_size+self.grid_size):
-            for j in range(x*self.grid_size,x*self.grid_size+self.grid_size):
-                color = self.screen.get_at((i, j)) 
-                if color==self.clear_color or color==self.obst_color :
-                    return True
-        self.change_color(self.explored_color,x,y)
-        return False
-            
-    def change_color(self, color, x, y):
-        cell_rect = pygame.Rect(y*self.grid_size, x*self.grid_size, self.grid_size, self.grid_size)
-        pygame.draw.rect(self.screen, self.grid_color, cell_rect, 1)
-        pygame.draw.rect(self.screen, color, cell_rect.inflate(-1, -1))
-        pygame.display.update()
+    
     
     def dijkstra(self):
         start_time = time.time()
         idx=1
         start=(0,0,0,self.START)
-        self.change_color(self.start_color,self.START[0],self.START[1])
-        goal=(self.H_PX+self.V_PX,0,0,self.GOAL)
+#         self.change_color(self.start_color,self.START[0],self.START[1])
+        goal=(float('inf'),None,None,self.GOAL)
         
         # Define action sets
         actions=[[0,1],[1,0],[0,-1],[-1,0],[1,1],[-1,-1],[-1,1],[1,-1]]
@@ -165,61 +222,58 @@ class Dijkstra:
         tracker=[]
         current=start
         open_list.put(start)
-        
         while open_list and not current[3]==goal[3]:
+            print('.',end=)
             current=open_list.get()
+            close_list.add(current[3])
             tracker.append(current)
-            if current[3] in close_list:
-                    continue
+#             if current[3] in close_list:
+#                     continue
             if current[3]==goal[3]:
                     break
-            close_list.add(current[3])
-            for a in actions:
-                    cost=current[0]
-                    parent_idx=current[2]
-                    neighbor=(current[3][0]+a[0],current[3][1]+a[1])
-                    x,y=neighbor
-                    if x>=0 and y>=0 and x<self.V_PX and y<self.H_PX and not self.check_grid(x,y):
-                            if neighbor in close_list:
-                                continue
-                            if a[0]==0 or a[1]==0:
-                                cost=cost+1
+            
+            else:
+                for a in actions:
+                    neighbor,cost_of_action=self.move(current[3],a)
+                    if not neighbor==None:
+                        
+                        if neighbor[3] not in close_list and not neighbor[0]==-1:
+                            if tuple(neighbor) not in open_list.queue or neighbor[0]==float('inf'):
+                                self.img[neighbor[3][0]][neighbor[3][1]]=self.explored_color
+                                flip_img = cv2.flip(self.img, 0)
+                                # cv2.imshow("Out",flip_img)
+                                # cv2.waitKey(1)
+                                self.result.write(flip_img)
+                        
+                                neighbor[0]=current[0]+cost_of_action
+                                neighbor[1]=current[2]
+                                idx=idx+1
+                                neighbor[2]=idx
+                                # print(neighbor)
+                                open_list.put(tuple(neighbor))
                             else:
-                                cost=cost+1.4
-                            idx=idx+1
-                            open_list.put((cost,parent_idx,idx,neighbor))
+                                if neighbor[0]>current[0]+cost_of_action:
+                                    neighbor[1]=current[2]
+                                    neighbor[0]=current[0]+cost_of_action
+                        
         end_time = time.time()
         elapsed_time = end_time - start_time
         print(f"Execution time of algorithm: {elapsed_time:.2f} seconds")
-        self.change_color(self.goal_color,self.GOAL[0],self.GOAL[1])
-        self.change_color(self.start_color,self.START[0],self.START[1])
         return tracker,current
-        
-    def back_track(self,tracker,current):
-        g_node=current
-        path=[]
-        while not g_node[2]==0:
-            for c in tracker:
-                if c[2] == g_node[1]:
-                    self.change_color(self.track_color ,c[3][0],c[3][1])
-                    path.append([c[3][0],c[3][1]])
-                    g_node=c
-        print("Complete")
-        path.reverse()
-        for x,y in path:
-            time.sleep(0.05)
-            pygame.draw.circle(self.screen, self.robot_color, (y*self.grid_size,x*self.grid_size), 5)
-            pygame.display.update()
 
 if __name__ == "__main__":
     # Define start and goal
     # Inverted co ordinate system compensation
-    goal=(0,239)
-    start=(99,0)
-    goal=(5,2)
-    start=(0,0)
+    # goal=(230,500)
+    sx=int(input("Enter X co ordinate of start point: "))
+    sy=int(input("Enter Y co ordinate of start point: "))
+    start=(sy,sx)
+    gx=int(input("Enter X co ordinate of goal point: "))
+    gy=int(input("Enter Y co ordinate of goal point: "))
+    goal=(gy,gx)
+    # start=(0,0)
     # Create an instance of Dijkstra
-    d_algo = Dijkstra(600,250,2,start,goal)
+    d_algo = Dijkstra(600,250,1,start,goal)
     
     # Call the game method
     d_algo.game()
---
+++ readme.md
@@ -0,0 +1,29 @@
+# ENPM 661
+
+## DIJKSTRA ALGORITHM
+
+### Student Name
+- Shantanu Parab
+
+### Student ID
+- sparab@umd.edu
+
+## Installation Instructions
+- To install numpy, run `pip install numpy` in your terminal.
+- To install opencv, run `pip install opencv-python` in your terminal.
+
+## Source Code Files
+- [`dijkstra.py`](dijkstra.py) - Contains the implementation of Dijkstra algorithm to solve the path planning problem.
+  + The program will run a dijkstra algorithm for path planning and create a video. This video will be outputted in the end.
+  + Once the program runs  completetly it will show  a image output of optimum path.
+  + Press enter once the image is shown this will complete the video recording
+  + The program will ask the user to give the inputs for start and goal and check wheter it is valid
+
+
+
+## How to Run the Program:
+To run the program, open the command prompt/terminal and navigate to the directory where the source code files are located. Then, type the following command: python [filename].py.
+
+
+
+
